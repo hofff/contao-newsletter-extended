ae504aa07dbb631860a835b5447fb3cd6c90e696
Cliff Parnitzky <github@cliff-parnitzky.de>
2016-03-16 22:36:49 +0100

    Added defining a custom page for activation confirmation

---
+++ CT_ROOT/system/modules/hofff_newsletter-extended/languages/en/tl_module.php
@@ -0,0 +1,3 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_module']['nlExtConfirmPage'] = array('Custom page for subscription confirmation', 'Please choose the page which is displayed to the subscriber when the subscription confirmation was completed via e-mail link.');
